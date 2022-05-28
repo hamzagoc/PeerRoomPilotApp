dab17c6959aa6d8a5a0a1da157fc5d30e1090e8e
Hamza Göç <hamzaburakhan@gmail.com>
2022-05-28 11:47:59 +0300

    refactor and ux improvement

--- src/App.js
+++ src/App.js
@@ -1,101 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import './App.css';
-import Room from './PeerRoom/service/Room';
-import Client from './PeerRoom/service/Client';
-import { makeid } from './util/Util';
+import ExampleRoom from './ExampleRoom/ExampleRoom';
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [clientName, setClientName] = useState("N/A");
-  const [history, _setHistory] = useState([]);
-  const historyRef = useRef([]);
-
-  const room = useRef();
-  const client = useRef();
-
-  useEffect(() => {
-    setClientName(makeid(5));
-  }, [])
-
-  function setHistory(data) {
-    historyRef.current = data;
-    _setHistory(data);
-  }
-
-  function addMessageToHistory(message) {
-    console.log(message);
-    setHistory([...historyRef.current, message]);
-  }
-
-  function createRoom() {
-    room.current = Room({
-      roomId: "room-id",
-      debug: true,
-      onRoomReady: (id) => {
-        addMessageToHistory("Room created: " + id);
-      },
-      onDataReceived: (connection, data) => {
-        const { username } = connection.metadata || {};
-        addMessageToHistory(username + ": " + JSON.stringify(data));
-      },
-      onClientConnectedToRoom: (connection) => {
-        const { username } = connection.metadata || {};
-        addMessageToHistory(username + " joined the room.");
-        room.current.broadcast({ data: username + " joined the room." })
-      }
-    });
-    room.current.init();
-  }
-
-  function createClientAndConnect() {
-    client.current = Client({
-      username: clientName,
-      debug: true,
-      onClientReady: (id) => {
-        client.current.connectToRoom("room-id");
-      },
-      onJoinTheRoom: () => {
-        addMessageToHistory("You joined the room");
-      },
-      onDataReceived: (connection, d) => {
-        const { username, data } = d;
-        const msg = username ? username + ": " + JSON.stringify(data) : JSON.stringify(data);
-        addMessageToHistory(msg);
-      }
-    });
-    client.current.init();
-  }
-
-  function handleMessageChange(event) {
-    setMessage(event.target.value);
-  }
-
-  function sendMessage() {
-    client.current.sendMessage(message);
-    addMessageToHistory("You: " + message);
-    setMessage("");
-  }
 
   return (
     <div className="App">
-      <h1>hello {clientName}</h1>
-      <button onClick={createRoom}>Create Room</button>
-      <button onClick={createClientAndConnect}>Join Room</button>
-      <br></br>
-      <div>
-        <ul>
-          {history.map((item, index) => {
-            return (<li key={index}>
-              {item}
-            </li>)
-          })}
-        </ul>
-
-      </div>
-
-      <input onChange={handleMessageChange} value={message} />
-      <button onClick={sendMessage}>Send Message</button>
-
+      <ExampleRoom />
     </div>
   );
 }
---
+++ src/ExampleRoom/ExampleRoom.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Room from '../PeerRoom/service/Room';
+import Client from '../PeerRoom/service/Client';
+import { makeid } from '../util/Util';
+
+function ExampleRoom() {
+    const [isRoom, setIsRoom] = useState(false);
+    const [isClient, setIsClient] = useState(false);
+    const [message, setMessage] = useState("");
+    const [clientName, setClientName] = useState("N/A");
+    const [history, _setHistory] = useState([]);
+    const historyRef = useRef([]);
+
+
+    const room = useRef();
+    const client = useRef();
+
+    useEffect(() => {
+        setClientName(makeid(5));
+    }, [])
+
+    function setHistory(data) {
+        historyRef.current = data;
+        _setHistory(data);
+    }
+
+    function addMessageToHistory(message) {
+        console.log(message);
+        setHistory([...historyRef.current, message]);
+    }
+
+    function createRoom() {
+        room.current = Room({
+            roomId: "room-id",
+            debug: true,
+            onRoomReady: (id) => {
+                addMessageToHistory("Room created: " + id);
+                setIsRoom(true);
+            },
+            onDataReceived: (connection, data) => {
+                const { username } = connection.metadata || {};
+                addMessageToHistory(username + ": " + JSON.stringify(data));
+            },
+            onClientConnectedToRoom: (connection) => {
+                const { username } = connection.metadata || {};
+                addMessageToHistory(username + " joined the room.");
+                room.current.broadcast({ data: username + " joined the room." })
+            }
+        });
+        room.current.init();
+    }
+
+    function createClientAndConnect() {
+        client.current = Client({
+            username: clientName,
+            debug: true,
+            onClientReady: (id) => {
+                client.current.connectToRoom("room-id");
+                setIsClient(true);
+            },
+            onJoinTheRoom: () => {
+                addMessageToHistory("You joined the room");
+            },
+            onDataReceived: (connection, d) => {
+                const { username, data } = d;
+                const msg = username ? username + ": " + JSON.stringify(data) : JSON.stringify(data);
+                addMessageToHistory(msg);
+            }
+        });
+        client.current.init();
+        setIsClient(true);
+    }
+
+    function handleMessageChange(event) {
+        setMessage(event.target.value);
+    }
+
+    function sendMessage() {
+        client.current.sendMessage(message);
+        addMessageToHistory("You: " + message);
+        setMessage("");
+    }
+
+
+    return (
+        <div className="App">
+            <h1>hello {clientName}</h1>
+            {(!isRoom && !isClient) &&
+                (<>
+                    <button onClick={createRoom}>Create Room</button>
+                    <button onClick={createClientAndConnect}>Join Room</button>
+                    <br></br>
+                </>)
+            }
+
+            <div>
+                <ul>
+                    {history.map((item, index) => {
+                        return (<li key={index}>
+                            {item}
+                        </li>)
+                    })}
+                </ul>
+
+            </div>
+
+            {isClient && <>
+                <input onChange={handleMessageChange} value={message} />
+                <button onClick={sendMessage}>Send Message</button>
+            </>}
+
+        </div>
+    );
+}
+
+
+export default ExampleRoom;
