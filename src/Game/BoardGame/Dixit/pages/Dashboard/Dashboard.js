150309d3d2f816d5e4b7325ea3c08c26cfd4a142
Hamza Göç <hamzaburakhan@gmail.com>
2022-05-30 01:23:12 +0300

    create user

--- src/Game/BoardGame/Dixit/pages/Dashboard/Dashboard.js
+++ src/Game/BoardGame/Dixit/pages/Dashboard/Dashboard.js
@@ -1,42 +1,104 @@
-import React from 'react';
-import { Tabs, Input, Button, Space } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Tabs, Input, Button, Space, Avatar } from 'antd';
 import { AppstoreOutlined, TeamOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import { ArrowLeftOutlined, ArrowRightOutlined, BgColorsOutlined } from '@ant-design/icons';
+import AvatarIcon from '../../../../constants/AvatarIcon';
+const colors = [
+    //red
+    "#ffa39e",
+    "#ff7875",
+    "#ff4d4f",
+    "#f5222d",
+    //volcano
+    "#ffbb96",
+    "#ff9c6e",
+    "#ff7a45",
+    "#fa541c",
+    //orange
+    "#ffd591",
+    "#ffc069",
+    "#ffa940",
+    "#fa8c16",
+    //gold
+    "#ffd666",
+    "#ffc53d",
+    "#faad14",
+    //yellow
+    "#fff566",
+    "#ffec3d",
+    "#fadb14",
+    //lime
+    "#d3f261",
+    "#bae637",
+    "#a0d911",
+    //green
+    "#b7eb8f",
+    "#95de64",
+    "#73d13d",
+    "#52c41a",
+    //cyan
+    "#87e8de",
+    "#5cdbd3",
+    "#36cfc9",
+    "#13c2c2",
+    //blue
+    "#91d5ff",
+    "#69c0ff",
+    "#40a9ff",
+    "#1890ff",
+    //magenta
+    "#ffadd2",
+    "#ff85c0",
+    "#f759ab",
+    "#eb2f96",
+    //purple
+    "#d3adf7",
+    "#b37feb",
+    "#9254de",
+    "#722ed1",
+];
 
 function DashBoard() {
+    const [activeTabKey, setActiveTabKey] = useState(TABS.CREATE_ROOM);
+    const [username, setUsername] = useState("")
+
+    useEffect(() => {
+        console.log("Created");
+    }, []);
 
     const handleOnChange = (activeTab) => {
-        console.log(activeTab);
+        setActiveTabKey(activeTab);
     }
 
     const handleUsernameChange = (e) => {
-        console.log(e.target.value);
-
+        setUsername(e.target.value);
     }
 
-    const TabPaneContent = ({ actionButtonText }) => {
-        return (
-            <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                <Input placeholder="Username" onChange={handleUsernameChange} />
-                <Button type="primary" style={{ float: 'right' }}>
-                    {actionButtonText}
-                </Button>
-            </Space>
-        );
-    }
     return (
         <Container>
-            <Tabs defaultActiveKey={TABS.CREATE_ROOM} onChange={handleOnChange}>
-                <Tabs.TabPane key={TABS.CREATE_ROOM} tab={<CreateRoomTab />}>
-                    <TabPaneContent actionButtonText="Create Room" />
-                </Tabs.TabPane>
-                <Tabs.TabPane key={TABS.JOIN_ROOM} tab={<JoinRoomTab />}>
-                    <TabPaneContent actionButtonText="Join Room" />
-                </Tabs.TabPane>
+            <Tabs defaultActiveKey={activeTabKey} onChange={handleOnChange}>
+                <Tabs.TabPane key={TABS.CREATE_ROOM} tab={<CreateRoomTab />} />
+                <Tabs.TabPane key={TABS.JOIN_ROOM} tab={<JoinRoomTab />} />
             </Tabs>
+
+            <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                <UserDetail>
+                    <AvatarSelector name={username} />
+                    <ActionGroup>
+                        <Input placeholder="Username" value={username} onChange={handleUsernameChange} />
+                        <div>
+                            <Button type="primary" style={{ float: 'right' }}>
+                                {activeTabKey === TABS.CREATE_ROOM ? "Create Room" : "Join Room"}
+                            </Button>
+                        </div>
+                    </ActionGroup>
+                </UserDetail>
+            </Space>
         </Container>
     );
 }
+
 
 const TABS = {
     CREATE_ROOM: "CREATE_ROOM",
@@ -61,10 +123,93 @@ const JoinRoomTab = () => {
     </>
 }
 
+const AvatarSelector = ({ name }) => {
+    const iconKeys = Object.keys(AvatarIcon);
+    const [selectedAvatar, setSelectedAvatar] = useState(getRandomAvatar());
+    const [avatarIndex, setAvatarIndex] = useState(0);
+    const [selectedColor, setSelectedColor] = useState(getRandomColor());
+
+    useEffect(() => {
+        setSelectedAvatar(AvatarIcon[iconKeys[avatarIndex]]);
+    }, [avatarIndex]);
+
+    function getRandomAvatar() {
+        const avatarIndex = randomNumber(colors.length - 1);
+        return AvatarIcon[iconKeys[avatarIndex]];
+    }
+
+    function getRandomColor() {
+        const colorIndex = randomNumber(colors.length - 1);
+        return colors[colorIndex];
+    }
+
+    function randomNumber(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function handlePreviousAvatar() {
+        const index = avatarIndex - 1;
+        if (index < 0) {
+            setAvatarIndex(iconKeys.length - 1);
+
+        } else {
+            setAvatarIndex(index);
+        }
+    }
+
+    function handleNextAvatar() {
+        const index = avatarIndex + 1;
+        if (index > iconKeys.length - 1) {
+            setAvatarIndex(0);
+        } else {
+            setAvatarIndex(index);
+        }
+    }
+
+    function handleRandomColor() {
+        setSelectedColor(getRandomColor());
+    }
+
+    return (
+        <AvatarSelectorContainer>
+            <Avatar style={{ backgroundColor: selectedColor }} icon={selectedAvatar} size={50} />
+            <span>{name || (<span style={{ color: 'gray' }}>Username</span>)}</span>
+            <SelectorButtons>
+                <Button type="primary" shape="circle" size="small" icon={<ArrowLeftOutlined />} onClick={handlePreviousAvatar} />
+                <Button type="primary" shape="circle" size="small" icon={<ArrowRightOutlined />} onClick={handleNextAvatar} />
+                <Button type="primary" shape="circle" size="small" icon={<BgColorsOutlined />} onClick={handleRandomColor} />
+            </SelectorButtons>
+        </AvatarSelectorContainer>
+    )
+};
+
 const Container = styled.div`
-  margin: auto;
-  max-width: 500px;
-  padding: 100px 20px;
+    margin: auto;
+    max-width: 500px;
+    padding: 100px 20px;
 `;
+const ActionGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const AvatarSelectorContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+`;
+
+const SelectorButtons = styled.div`
+    display: flex;
+    gap: 5px;
+`;
+
+const UserDetail = styled.div`
+    display: flex;
+    gap: 36px;
+`;
+
 
 export default DashBoard;
---
+++ src/Game/constants/AvatarIcon.js
@@ -0,0 +1,35 @@
+
+import {
+    SmileFilled, RocketFilled, StarFilled, FireFilled,
+    MehFilled, SketchCircleFilled, ExperimentFilled,
+    EyeFilled, BugFilled, FrownFilled, HeartFilled, ReadFilled,
+    QqCircleFilled, CarFilled, GitlabFilled, GithubFilled,
+    CloudFilled, ThunderboltFilled, DingtalkCircleFilled, RobotFilled
+
+} from '@ant-design/icons';
+
+const AvatarIcon = {
+    SMILE: <SmileFilled />,
+    MEH: <MehFilled />,
+    ROCKET: <RocketFilled />,
+    DIOMAND: <SketchCircleFilled />,
+    STAR: <StarFilled />,
+    FOX: <GitlabFilled />,
+    LAB: <ExperimentFilled />,
+    EYE: <EyeFilled />,
+    BUG: <BugFilled />,
+    FIRE: <FireFilled />,
+    FROWN: <FrownFilled />,
+    HEART: <HeartFilled />,
+    PENGUEN: <QqCircleFilled />,
+    CAR: <CarFilled />,
+    CAT: <GithubFilled />,
+    CLOUD: <CloudFilled />,
+    WING: <DingtalkCircleFilled />,
+    THUNDERBOLD: <ThunderboltFilled />,
+    ROBOT: <RobotFilled />,
+    READ: <ReadFilled />,
+
+}
+
+export default AvatarIcon;
